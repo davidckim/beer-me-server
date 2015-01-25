49dd563ebb05f70b8a8e36a90946707d5c6eae36
Stephen Clem <stevebclem@gmail.com>
2015-01-25 14:53:39 -0800

    Complete database

--- db/seeds.rb
+++ db/seeds.rb
@@ -74,21 +74,23 @@ while counter <= 5 do
       @zip_code = @location_info.match(/\d{5}/).to_s
 
       beers_in_restaurants.css('a').each do |info|
-        if info.to_s.include?('Draft')
+        # if info.to_s.include?('Draft')
           @beer = info.attribute('data-screen-title').to_s
-          if @existing_beer = Beer.find_by(name: @beer)
-            @existing_beer.locations << Location.create({name: @restaurant, address: @address, zip_code: @zip_code})
-          else
             if @beer != ''
               info = info.css('p').text.strip.gsub("\n", "").split('  ')
               # @type = info[0]
               @size = info[6].to_s
               @abv = info[2]
-              @price = info.last.strip.to_s[3..-1]
-              # if @size.include?('Draft') || @size.include?('draft')
-                puts "#{@size}!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+              @price = info.last.strip.to_s[4..-1]
+              if @size.include?('Draft')
+                # puts "#{@size}!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
                 @new_beer = Beer.create(name: @beer, abv: @abv, price: @price)
-                @new_beer.locations << Location.create({name: @restaurant, address: @address, zip_code: @zip_code})
+                if @existing_beer = Beer.find_by(name: @beer)
+                  if !@existing_beer.locations.find_by(name: @restaurant)
+                    @existing_beer.locations << Location.create({name: @restaurant, address: @address, zip_code: @zip_code})
+                  end
+                # @new_beer.locations << Location.create({name: @restaurant, address: @address, zip_code: @zip_code})
+              else
             end
           end
         end
